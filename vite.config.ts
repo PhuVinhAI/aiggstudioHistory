de1654f95b769cfe1e2867cf2d39ffe47c5cd4eb
TomiWixoss <kotoricktv@gmail.com>
2026-03-11 09:25:34 +0700

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 import manifest from './manifest.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), tailwindcss(), crx({ manifest })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
